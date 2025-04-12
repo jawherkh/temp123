e3952c37beab8932ad649c11c4d0f247882fef0c
Jawher Khalifa <jawherkhalifa891@gmail.com>
2025-04-12 19:14:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/components/icons.ts:2:1
    > Export WaterDrop doesn't exist in target module
    >   1 |
    > > 2 | import {ArrowRight, Check, ChevronsUpDown, Circle, Copy, Edit, ExternalLink, File, HelpCircle, Home, Loader2, Mail, MessageSquare, Moon, Plus, PlusCircle, Search, Server, Settings, Share2, Shield, Sun, Trash, User, WaterDrop, X, Workflow} from 'lucide-react';
    >     | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    >   3 |
    >   4 | const Icons = {
    >   5 |   arrowRight: ArrowRight,
    >
    > The export WaterDrop was not found in module [project]/node_modules/lucide-react/dist/esm/lucide-react.js [app-client] (ecmascript) <exports>.
    > Did you mean to import Carrot?
    > All exports of the module are statically known (It doesn't have dynamic exports). So it's known statically that the requested export doesn't exist.
    >
    >

--- src/components/icons.ts
+++ src/components/icons.ts
@@ -1,5 +1,5 @@
 
-import {ArrowRight, Check, ChevronsUpDown, Circle, Copy, Edit, ExternalLink, File, HelpCircle, Home, Loader2, Mail, MessageSquare, Moon, Plus, PlusCircle, Search, Server, Settings, Share2, Shield, Skin, Sun, Trash, User, WaterDrop, X, Workflow} from 'lucide-react';
+import {ArrowRight, Check, ChevronsUpDown, Circle, Copy, Edit, ExternalLink, File, HelpCircle, Home, Loader2, Mail, MessageSquare, Moon, Plus, PlusCircle, Search, Server, Settings, Share2, Shield, Sun, Trash, User, X, Workflow} from 'lucide-react';
 
 const Icons = {
   arrowRight: ArrowRight,
@@ -29,8 +29,6 @@ const Icons = {
   spinner: Loader2,
   trash: Trash,
   user: User,
-  skin: Skin,
-  waterDrop: WaterDrop,
 };
 
 export {Icons};
